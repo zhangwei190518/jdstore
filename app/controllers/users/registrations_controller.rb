864edc8c9caaff5357c896198d23cb85767b16da
zhangwei <zhangwei@otcbtc.com>
2018-12-19 14:13:29 +0800

    user can sign up to seller or customer

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:is_admin, :mobile])
+    end
+end
