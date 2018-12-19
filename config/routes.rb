e60bd4af6fd1356f17c2704710fa5a0dd173ae6e
zhangwei <zhangwei@otcbtc.com>
2018-12-19 15:15:31 +0800

    seller can create/edit product

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers:{registrations: 'users/registrations'}
+
+  namespace :admin do
+    resources :products
+  end
+
   root 'welcome#index'
 end
