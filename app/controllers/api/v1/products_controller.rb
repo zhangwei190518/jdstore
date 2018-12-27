948433dcedbee5cff494a8844cdf8d527cac89fd
zhangwei <zhangwei@otcbtc.com>
2018-12-27 16:50:42 +0800

    add api/v1/products/show api

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::ProductsController < ::ProductsController
   def index
     super
   end
+
+  def show
+    super
+  end
 end
---
+++ app/views/api/v1/pictures/_picture.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! picture, :id, :avatar_url, :created_at, :updated_at
