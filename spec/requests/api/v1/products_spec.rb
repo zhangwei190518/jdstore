c063a0266093193f7c6cc5c1769203cbee3796a9
zhangwei <zhangwei@otcbtc.com>
2018-12-28 14:34:39 +0800

    modify api/v1/products/show spec

--- spec/requests/api/v1/products_spec.rb
+++ spec/requests/api/v1/products_spec.rb
@@ -82,8 +82,8 @@ RSpec.describe "Products", type: :request do
       get "/api/v1/products/#{product.id}", params: { format: :json }
 
       result = JSON.parse(response.body)
-      expect(result["title"]).to eq "iPhone"
-      expect(result["description"]).to eq "新一代 iPhone"
+      expect(result["title"]).to eq product.title
+      expect(result["description"]).to eq product.description
       expect(result["quantity"]).to eq product.quantity
       expect(result["price"]).to eq product.price
       expect(result["is_hidden"]).to be_falsey
